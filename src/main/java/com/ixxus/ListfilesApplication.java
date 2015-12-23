4786eabf89abfb958750623d58497a2e3520cd88
Nazareth <nazareth@Nazareths-MacBook-Pro.local>
2015-12-23 12:46:00 +0100

    Project Creation

---
+++ src/main/java/com/ixxus/ListfilesApplication.java
@@ -0,0 +1,12 @@
+package com.ixxus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListfilesApplication.class, args);
+    }
+}
